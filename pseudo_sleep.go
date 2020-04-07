429fe685bddd0f363196519fa440af8fa3759b4b
upuneetu <upuneetu@gmail.com>
2020-04-07 22:33:32 +0530

    added pseudo_sleep.go (timeout after 5s)

---
+++ pseudo_sleep.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func forever(c chan byte) {
+	var input byte
+	for {
+		fmt.Scanln(&input)
+		c <- input
+	}
+}
+
+func main() {
+
+	c := make(chan byte)
+	go forever(c)
+
+	sleep := false
+
+	for !sleep {
+		select {
+		case <-c:
+			continue
+		case <-time.After(time.Second * 5):
+			sleep = true
+		}
+	}
+
+}
